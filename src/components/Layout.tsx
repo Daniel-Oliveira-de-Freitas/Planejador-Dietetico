ef2c91bb92e51b09a1a994f0f36a543adf611f55
William Oliveira <48337511+willraoli@users.noreply.github.com>
2022-12-03 19:34:58 -0300

    feat: add layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom';
+
+interface NavbarProps {
+  children: React.ReactNode;
+}
+const Layout = (props: NavbarProps) => {
+  const APP_NAME = 'Planejador';
+  const NAV_ITEMS = [
+    {
+      label: 'Início',
+      href: '/',
+    },
+    {
+      label: 'Pacientes',
+      href: '#',
+    },
+    {
+      label: 'Consumo habitual',
+      href: '/consumohabitual',
+    },
+    {
+      label: 'Consumo 24h',
+      href: '/consumo24h',
+    },
+  ];
+
+  return (
+    <div className='flex h-screen'>
+      <aside className='w-48 border-r-2 bg-neutral-100'>
+        <h2 className='text-lg font-bold pl-2 my-2'>{APP_NAME}</h2>
+        <ul>
+          {NAV_ITEMS.map(item => (
+            <NavItem
+              key={item.href}
+              href={item.href}
+            >
+              {item.label}
+            </NavItem>
+          ))}
+        </ul>
+      </aside>
+      <main className='p-4 w-full'>{props.children}</main>
+    </div>
+  );
+};
+
+interface NavItemProps {
+  children: string;
+  href: string;
+}
+
+const NavItem = (props: NavItemProps) => {
+  return (
+    <Link to={props.href}>
+      <li className='py-1 hover:bg-neutral-200 pl-2'>{props.children}</li>
+    </Link>
+  );
+};
+
+export default Layout;
