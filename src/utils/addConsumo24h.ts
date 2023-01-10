960ef4e6211592047e671dfeee19988e7beb0177
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-10 16:07:51 -0300

    feat (wip): add addConsumo24h utils

---
+++ src/utils/addConsumo24h.ts
@@ -0,0 +1,21 @@
+import { RefeicaoConsumo24h } from '@prisma/client';
+
+export const addConsumo24h = async (
+  refeicoes: RefeicaoConsumo24h[],
+  pacienteId: number
+) => {
+  await window.prisma.consumo24h.create({
+    data: {
+      refeicoes: {
+        create: refeicoes,
+      },
+    },
+    select: {
+      Paciente: {
+        where: {
+          id: pacienteId,
+        },
+      },
+    },
+  });
+};
