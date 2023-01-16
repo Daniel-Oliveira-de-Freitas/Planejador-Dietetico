d3f75bce7d84b9bba8195a8f16aece407f7dd710
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-16 15:30:48 -0300

    refactor: include refeicoes when fetching consumo24h

--- src/utils/getConsumo24h.ts
+++ src/utils/getConsumo24h.ts
@@ -1,5 +1,10 @@
 export const getConsumo24h = async (pacienteId: number) => {
-  const consumo24h = await window.prisma.consumo24h.findUnique({ where: { id: pacienteId } });
+  const consumo24h = await window.prisma.consumo24h.findUnique({
+    where: { id: pacienteId },
+    include: {
+      refeicoes: true,
+    },
+  });
 
   return consumo24h;
 };
