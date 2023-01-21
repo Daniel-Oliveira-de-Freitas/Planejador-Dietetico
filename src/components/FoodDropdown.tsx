3814ed8d3b226d3054dfc80052988d01cacaed0d
William Oliveira <william.christopher.96@gmail.com>
2023-01-21 14:45:37 -0300

    fix(FoodDropdown): show macros according to measure

--- src/components/FoodDropdown.tsx
+++ src/components/FoodDropdown.tsx
@@ -1,6 +1,7 @@
 import { Refeicao } from '../types/types';
 import React from 'react';
 import { TipoDeRefeicao } from '@prisma/client';
+import { Button } from './Button';
 
 interface FoodDropdownProps {
   setIsOpen: (value: React.SetStateAction<boolean>) => void;
@@ -16,15 +17,14 @@ const FoodDropdown = (props: FoodDropdownProps) => {
         <details key={tipoDeRefeicao.id}>
           <summary className='group mt-2 flex items-center justify-between rounded-xl border border-gray-200 py-2 px-4 text-left font-medium text-gray-500 hover:cursor-pointer hover:bg-gray-400 focus:ring-4 focus:ring-gray-200 group-first:mt-4'>
             <div className='group-hover:text-white'>{tipoDeRefeicao.nome}</div>
-            <button
+            <Button
               onClick={() => {
                 props.setIsOpen(true);
                 props.setTipoDeRefeicao(tipoDeRefeicao);
               }}
-              className='flex-end mr-2 mb-2 rounded-full bg-blue-500 px-5 py-2.5 text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300'
             >
               +
-            </button>
+            </Button>
           </summary>
           <table className='w-full rounded-b-lg text-left text-sm text-gray-500'>
             <thead className='bg-gray-50 text-xs uppercase text-gray-700'>
@@ -57,19 +57,20 @@ const FoodDropdown = (props: FoodDropdownProps) => {
                           {alimento.quantidade} {alimento.alimentoPinheiro.measures[0].label}
                         </td>
                         <td className='whitespace-nowrap py-4 px-6 font-medium text-gray-900'>
-                          {Math.ceil(alimento.alimentoTACO.energy[0].kcal)}
+                          {Math.ceil(alimento.alimentoTACO.energy[0].kcal) * alimento.quantidade}
                         </td>
                         <td className='whitespace-nowrap py-4 px-6 font-medium text-gray-900'>
-                          {Math.ceil(alimento.alimentoTACO.carbohydrate[0].qty) +
+                          {Math.ceil(alimento.alimentoTACO.carbohydrate[0].qty) *
+                            alimento.quantidade +
                             alimento.alimentoTACO.carbohydrate[0].unit}
                         </td>
                         <td className='whitespace-nowrap py-4 px-6 font-medium text-gray-900'>
-                          {Math.ceil(alimento.alimentoTACO.protein[0].qty) +
-                            alimento.alimentoTACO.protein[0].unit}
+                          {Math.ceil(alimento.alimentoTACO.protein[0].qty) * alimento.quantidade +
+                            alimento.alimentoTACO.protein[0].unit}{' '}
                         </td>
                         <td className='whitespace-nowrap py-4 px-6 font-medium text-gray-900'>
-                          {Math.ceil(alimento.alimentoTACO.lipid[0].qty) +
-                            alimento.alimentoTACO.lipid[0].unit}
+                          {Math.ceil(alimento.alimentoTACO.lipid[0].qty) * alimento.quantidade +
+                            alimento.alimentoTACO.lipid[0].unit}{' '}
                         </td>
                       </tr>
                     );
