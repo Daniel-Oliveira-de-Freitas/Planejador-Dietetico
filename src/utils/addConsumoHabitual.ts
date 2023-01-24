2ccede13b3dd0f035afa3a21a9d567a1ce4457aa
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-24 12:15:44 -0300

    refactor(addConsumoHabitual): upsert instead of create

--- src/utils/addConsumoHabitual.ts
+++ src/utils/addConsumoHabitual.ts
@@ -18,10 +18,46 @@ interface ConsumoHabitual {
 }
 
 export const addConsumoHabitual = async (pacienteId: number, data: ConsumoHabitual) => {
-  await window.prisma.consumoHabitual.create({
-    data: {
-      ...data,
-      Paciente: {
+  await window.prisma.consumoHabitual.upsert({
+    where: {
+      pacienteId,
+    },
+    update: {
+      acucar: data.acucar,
+      acucarFreq: data.acucarFreq,
+      adocante: data.adocante,
+      adocanteFreq: data.adocanteFreq,
+      carneComGordura: data.carneComGordura,
+      carneComGorduraFreq: data.carneComGorduraFreq,
+      coposDeAgua: data.coposDeAgua,
+      frangoComPele: data.frangoComPele,
+      frangoComPeleFreq: data.frangoComPeleFreq,
+      frituras: data.frituras,
+      friturasFreq: data.friturasFreq,
+      latasDeOleo: data.latasDeOleo,
+      localDaJanta: data.localDaJanta,
+      localDoAlmoco: data.localDoAlmoco,
+      quemPrepara: data.quemPrepara,
+      numeroDePessoas: data.numeroDePessoas,
+    },
+    create: {
+      acucar: data.acucar,
+      acucarFreq: data.acucarFreq,
+      adocante: data.adocante,
+      adocanteFreq: data.adocanteFreq,
+      carneComGordura: data.carneComGordura,
+      carneComGorduraFreq: data.carneComGorduraFreq,
+      coposDeAgua: data.coposDeAgua,
+      frangoComPele: data.frangoComPele,
+      frangoComPeleFreq: data.frangoComPeleFreq,
+      frituras: data.frituras,
+      friturasFreq: data.friturasFreq,
+      latasDeOleo: data.latasDeOleo,
+      localDaJanta: data.localDaJanta,
+      localDoAlmoco: data.localDoAlmoco,
+      quemPrepara: data.quemPrepara,
+      numeroDePessoas: data.numeroDePessoas,
+      paciente: {
         connect: {
           id: pacienteId,
         },
