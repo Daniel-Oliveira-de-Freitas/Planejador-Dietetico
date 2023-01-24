cba1fb8a7e2f30eba4e188b87f40ad2b33ebf52d
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-24 12:14:48 -0300

    refactor(getPaciente): remove consumoHabitual from query

--- src/utils/paciente/getPaciente.ts
+++ src/utils/paciente/getPaciente.ts
@@ -1,12 +1,7 @@
 export const getPaciente = async (id: number) => {
-  const paciente = await window.prisma.paciente.findUnique({
+  return window.prisma.paciente.findUnique({
     where: {
       id,
     },
-    include: {
-      consumoHabitual: true,
-    },
   });
-
-  return paciente;
 };
