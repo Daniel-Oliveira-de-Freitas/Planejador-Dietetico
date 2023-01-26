91bd2b1689c438945161a06fdef5af295aa4ea85
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-25 23:16:54 -0300

    feat(Button): add form and rest props to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,23 @@
+import React from 'react';
+
 interface ButtonProps {
   type?: 'button' | 'reset' | 'submit';
   children: React.ReactNode;
   disabled?: boolean;
   className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  form?: string;
 }
 
-export const Button = ({ type, className, children, disabled, onClick, ...rest }: ButtonProps) => {
+export const Button = ({
+  type,
+  className,
+  children,
+  form,
+  disabled,
+  onClick,
+  ...rest
+}: ButtonProps) => {
   const defaultStyles =
     'rounded-md bg-sky-600 py-2 px-4 text-lg font-semibold text-white hover:bg-sky-700';
 
@@ -16,6 +27,8 @@ export const Button = ({ type, className, children, disabled, onClick, ...rest }
       className={className ? `${defaultStyles} ${className}` : defaultStyles}
       disabled={disabled}
       onClick={onClick}
+      form={form}
+      {...rest}
     >
       {children}
     </button>
