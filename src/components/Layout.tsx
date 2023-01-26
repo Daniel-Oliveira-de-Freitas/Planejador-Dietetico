86555fd3e026bdc3e6002580bb65a38741023e5a
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-26 14:36:15 -0300

    feat(Layout): new layout component
    Removed sidebar and added top navigation bar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,58 +5,21 @@ interface NavbarProps {
   children: React.ReactNode;
 }
 const Layout = (props: NavbarProps) => {
-  const APP_NAME = '😋Planejador';
-  const NAV_ITEMS = [
-    {
-      label: '🏠 Início',
-      href: '/',
-    },
-    {
-      label: '👩 Pacientes',
-      href: '/pacientes',
-    },
-  ];
-
-  return (
-    <div className='flex'>
-      <aside className='fixed h-screen w-64 border-r-2 bg-sky-600'>
-        <h2 className='mt-8 mb-8 pl-2 text-2xl font-bold text-white'>{APP_NAME}</h2>
-        <ul>
-          {NAV_ITEMS.map((item, index) => (
-            <div key={item.href}>
-              {index === 0 && <hr className='opacity-20' />}
-              <NavItem href={item.href}>{item.label}</NavItem>
-              <hr className='opacity-20' />
-            </div>
-          ))}
-        </ul>
-      </aside>
-      <main className='ml-64 w-full p-8'>{props.children}</main>
-    </div>
-  );
-};
-
-interface NavItemProps {
-  children: string;
-  href: string;
-}
-
-const NavItem = (props: NavItemProps) => {
   const location = useLocation();
-
   return (
-    <>
-      <Link to={props.href}>
-        <button
-          type={'button'}
-          className={`w-full py-4 pl-2 text-left text-lg font-semibold uppercase text-white transition-all hover:bg-sky-700 ${
-            location.pathname === props.href && 'bg-sky-700'
-          }`}
-        >
-          {props.children}
-        </button>
-      </Link>
-    </>
+    <div className='flex flex-col'>
+      <nav className={'flex h-[60px] w-full items-center bg-sky-600'}>
+        {location.pathname !== '/' && (
+          <Link to={'/'}>
+            <button className={'h-full p-4 text-xl text-white hover:bg-sky-700'}>🡐</button>
+          </Link>
+        )}
+        <div className={'flex w-full justify-center'}>
+          <h2 className={'text-xl text-white'}>😋 Planejador dietético</h2>
+        </div>
+      </nav>
+      <main className='mx-auto w-full p-8'>{props.children}</main>
+    </div>
   );
 };
 
