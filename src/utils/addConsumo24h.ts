85a3f77b41a6d8dcb03fce0ee0644f6209aabe52
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-25 23:15:53 -0300

    refactor(addConsumo24h): make addConsumo24h use only one Refeicao at a time

--- src/utils/addConsumo24h.ts
+++ src/utils/addConsumo24h.ts
@@ -1,46 +1,30 @@
 import { Paciente } from '@prisma/client';
 import { Refeicao } from '../types/types';
 
-export const addConsumo24h = async (consumos: Refeicao[], paciente: Paciente) => {
-  const transactions: any[] = [];
-
-  consumos.map(consumo => {
-    const createRefeicao = window.prisma.refeicaoConsumo24h.create({
-      data: {
-        medida: consumo.medida,
-        quantidade: consumo.quantidade,
-        alimentoPinheiro: {
-          connect: {
-            id: consumo.alimentoPinheiro.id,
-          },
-        },
-        alimentoTACO: {
-          connect: {
-            id: consumo.alimentoTACO.id,
-          },
-        },
-        tipoDeRefeicao: {
-          connect: {
-            id: consumo.tipoDeRefeicaoId,
-          },
-        },
-        Paciente: {
-          connect: {
-            id: paciente.id,
-          },
+export const addConsumo24h = async (consumo: Refeicao, paciente: Paciente) => {
+  await window.prisma.refeicaoConsumo24h.create({
+    data: {
+      medida: consumo.medida,
+      quantidade: consumo.quantidade,
+      alimentoPinheiro: {
+        connect: {
+          id: consumo.alimentoPinheiro.id,
         },
       },
-    });
-    transactions.push(createRefeicao);
-  });
-
-  const refeicoes: Refeicao[] = await window.prisma.$transaction(transactions);
-  const refeicoesIds = refeicoes.map(arr => arr.id);
-
-  await window.prisma.refeicaoConsumo24h.deleteMany({
-    where: {
-      id: {
-        notIn: refeicoesIds,
+      alimentoTACO: {
+        connect: {
+          id: consumo.alimentoTACO.id,
+        },
+      },
+      tipoDeRefeicao: {
+        connect: {
+          id: consumo.tipoDeRefeicaoId,
+        },
+      },
+      Paciente: {
+        connect: {
+          id: paciente.id,
+        },
       },
     },
   });
