034417e657158b6c407ff1a9ad2c5a4e0ce47691
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-01-26 21:48:30 -0300

    feat(Layout): use context for paciente

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,24 @@
 import { Link, useLocation } from 'react-router-dom';
-import React from 'react';
+import React, { useContext } from 'react';
+import { PacienteContext } from '../context/PacienteContext';
 
 interface NavbarProps {
   children: React.ReactNode;
 }
 const Layout = (props: NavbarProps) => {
   const location = useLocation();
+  const { setTab } = useContext(PacienteContext);
   return (
     <div className='flex flex-col'>
       <nav className={'flex h-[60px] w-full items-center bg-sky-600'}>
         {location.pathname !== '/' && (
           <Link to={'/'}>
-            <button className={'h-full p-4 text-xl text-white hover:bg-sky-700'}>🡐</button>
+            <button
+              className={'h-full p-4 text-xl text-white hover:bg-sky-700'}
+              onClick={() => setTab(0)}
+            >
+              🡐
+            </button>
           </Link>
         )}
         <div className={'flex w-full justify-center'}>
